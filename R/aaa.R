e0ff702cc0de6823f5d38188bd1f02df1d1c4169
Chitu Okoli <Chitu.Okoli@skema.edu>
2025-03-03 09:51:17 +0100

    build: Suppress package startup message while developing

--- R/aaa.R
+++ R/aaa.R
@@ -7,9 +7,18 @@
   methods_register()
 }
 
+
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("The 'get()' function for 'ale' works such that the masked base::get() still works fine without modification.")
+  dev_load_all <- sys.calls() |>
+    as.character() |>
+    stringr::str_detect('load_all') |>
+    any()
+
+  if (!dev_load_all) {
+    packageStartupMessage("The 'get()' function for 'ale' works such that the masked base::get() still works fine without modification.")
+  }
 }
+
 
 # Create a package-wide environment to hold objects shared across functions.
 # Its current use is only for objects that need to be reused across random iterations for ALEpDist().
