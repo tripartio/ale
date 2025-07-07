c9885aa732b6c1304bde94b6b349fa28546e67b4
tripartio <Chitu.Okoli@skema.edu>
2025-07-07 22:44:05 +0200

    fix: layers should inherit ggproto

--- R/ALEPlots_methods.R
+++ R/ALEPlots_methods.R
@@ -353,14 +353,14 @@ validate_ALEPlots_method_args <- function(
 #' @description
 #' Customize an `ALEPlots` object by modifying plots indicated by the combination of `x_cols`, `type`, and `cats` as specified. Some arguments indicate some common customizations such as zooming in or out; see the argument documentation for available simple options.
 #'
-#' The most flexible option is to specify a list of `ggplot` geoms with the `geoms` argument; this appends the provided geoms to each plot by applying the [ggplot2::+.gg()] method to them. Thus, any customization supported by appending `ggplot` geoms can be applied. If both `geoms` and simple options like `zoom_y` are specified, then the `geoms` layers are applied first and then any other option is applied in the order presented in the argument list. For full control over the order of customizations, only provide `geoms`.
+#' The most flexible option is to specify a list of `ggplot` layers with the `layers` argument; this appends the provided layers to each plot by applying the [ggplot2::+.gg()] method to them. Thus, any customization supported by appending `ggplot` layers can be applied. If both `layers` and simple options like `zoom_y` are specified, then the `layers` layers are applied first and then any other option is applied in the order presented in the argument list. For full control over the order of customizations, only provide `layers`.
 #'
 #' See [get.ALE()] for explanation of parameters not described here.
 #'
 #' @param plots_obj ALEPlots object to customize.
 #' @param type See documentation for [get.ALE()]
 #' @param cats See documentation for [get.ALE()]
-#' @param geoms List of `ggplot` geoms. These are appended to each plot indicated by the combination of `x_cols`, `type`, and `cats` by applying the `ggplot2` `+` operator to them.
+#' @param layers List of `ggplot` layers. These are appended to each plot indicated by the combination of `x_cols`, `type`, and `cats` by applying the `ggplot2` `+` operator to them.
 #' @param zoom_x,zoom_y numeric(2). Zoom the specified plots in or out to match the specified x or y limits, respectively. Must be a two-element numeric vector where the first element <= the second. Default `NULL` does not zoom.
 #'
 #' @returns An `ALEPlots` object where elements specified by x_cols and exclude_cols are modified accordingly. Non-specified elements are not modified.
@@ -372,7 +372,7 @@ customize <- function(
     exclude_cols = NULL,
     type = 'ale',
     cats = NULL,
-    geoms = NULL,
+    layers = NULL,
     zoom_x = NULL,
     zoom_y = NULL
 ) {
@@ -400,7 +400,8 @@ customize <- function(
 
   validate(
     # A LayerInstance is a ggplot layer object
-    is.null(geoms) || inherits(geoms, 'LayerInstance')
+    # I think ggproto is the correct root class: https://ggplot2-book.org/internals.html#sec-ggproto
+    is.null(layers) || inherits(layers, 'ggproto')
   )
 
   validate(
@@ -416,8 +417,7 @@ customize <- function(
 
   ## Create customization layers ------------
 
-  custom_layers <- geoms %||% list()
-  # custom_layers <- geoms %||% geom_blank()
+  custom_layers <- layers %||% list()
 
   # Add zoom layers
   if (!is.null(zoom_x) || !is.null(zoom_y)) {
